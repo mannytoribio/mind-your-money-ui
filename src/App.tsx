2b80bc1fb1c671633e77ac4a99ca211348fdae09
Manny Toribio <manny.mtoribio@gmail.com>
2021-11-11 12:01:36 -0500

    initial commit for the ui with user context

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router,
+Routes, 
+Route } from 'react-router-dom'
+import { ThemeProvider } from '@emotion/react'
+import { initializeApp } from 'firebase/app'
+import { createTheme, CssBaseline } from '@mui/material'
+import { Home } from './scenes/Home'
+import './App.css'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDWW7bh5I9sXZl6uzBST5TzntzSBhdQe9E",
+  authDomain: "mind-your-money-webapp.firebaseapp.com",
+  projectId: "mind-your-money-webapp",
+  storageBucket: "mind-your-money-webapp.appspot.com",
+  messagingSenderId: "242308773810",
+  appId: "1:242308773810:web:fa8fab2398c1f166db16ab"
+}
+
+const app = initializeApp(firebaseConfig)
 
 function App() {
+  const theme = createTheme({
+    palette: { mode: 'dark',
+  }
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route path="/" element={< Home />}/>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
-export default App;
+export default App
---
+++ src/context/UserContext.tsx
@@ -0,0 +1,26 @@
+import { createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useState } from "react";
+import { getAuth, User, onAuthStateChanged } from 'firebase/auth'
+
+interface UserContextT {
+  user?: User
+  setUser: Dispatch<SetStateAction<User>>
+}
+
+export const UserContext = createContext<UserContextT>({} as any)
+
+export const UserContextProvider = (props: {
+  children: ReactNode | ReactNode[]
+}) => {
+  const auth = getAuth()
+  const [user, setUser] = useState<User>(auth.currentUser!)
+  const value = { user, setUser } as UserContextT
+  const { Provider } = UserContext
+  onAuthStateChanged(auth, (u) => {
+    setUser(u!)
+  })
+  return <Provider value={value}>{props.children}</Provider>
+}
