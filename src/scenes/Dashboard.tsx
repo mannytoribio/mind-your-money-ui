0c86e6cd3332f80dd82ea80bbb5584d58b18e79c
Manny Toribio <manny.mtoribio@gmail.com>
2021-11-22 14:59:49 -0500

    started dashboard page with math

--- src/scenes/Dashboard.tsx
+++ src/scenes/Dashboard.tsx
@@ -1,7 +1,38 @@
-
+import { useContext, useState, useEffect } from "react"
+import { UserContext } from "../context/UserContext"
+import { getIncomeByUserId, Income } from "../service/income.service"
 
 export const Dashboard = () => {
+  const { user } = useContext(UserContext)
+  const [incomeItems, setIncomeItems] = useState<Income[]>()
+  const [totalIncome, setTotalIncome] = useState<number>(0)
+  useEffect(() => {
+    if (user) {
+      user
+        .getIdToken()
+        .then((jwt) => getIncomeByUserId(jwt, user.uid))
+        .then(setIncomeItems)
+    }
+  }, [])
+  // TODO: useEffects for JWT, expenses, goals, etc.
+  useEffect(() => { 
+    if (incomeItems && incomeItems.length) {
+      setTotalIncome(
+        incomeItems.reduce(
+          (totalIncome, income) =>
+            totalIncome + income.incomeAmount * income.incomeFrequency,
+          0
+        )
+      )
+    }
+  }, [incomeItems])
+  if (!incomeItems) {
+    return <>...loading</>
+  }
   return (
-    <h1>Dashboard</h1>
+    <>
+      <h1>Dashboard</h1>
+      <h2>{totalIncome}</h2>
+    </>
   )
 }
--- src/service/income.service.ts
+++ src/service/income.service.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export interface Income {
   _id?: string;
-  incomeStream: string;
   incomeAmount: number;
+  incomeStream: string;
   incomeFrequency: number;
   updated_at?: Date
   uid?: string
@@ -13,9 +13,10 @@ export const createIncome = async (income: Income) => {
   await axios.post('http://localhost:5000/income', income)
 }
 
-export const getIncomeById = async (_id: string) => {
-  const ret = await axios.get(`http://localhost:5000/income/:${_id}`)
+export const getIncomeByUserId = async (jwt: string, uid: string) => {
+  const ret = await axios.get(`http://localhost:5000/income/${uid}`, {
+    headers: { Authorization: jwt },})
 
-  return ret.data as Income
+  return ret.data as Income[]
 }
 
